bac4d755ecf831b7f56f98ecca3bb11216f17569
paolofullone <paolo.enrico@gmail.com>
2023-03-03 08:11:54 -0300

    sdt::cout em outra sintaxe

--- Marcos/Basica_Avancada/PrimeiroCodigo/Primeiro.cpp
+++ Marcos/Basica_Avancada/PrimeiroCodigo/Primeiro.cpp
@@ -8,9 +8,17 @@ int main()
 	std::cout << "Paolo Enrico Iacono Fullone - 44 anos" << std::endl;
 	// cout é o comando de saída de dados, neste caso está solicitando para imprimir na tela o texto entre aspas duplas
 	// endl é o comando de quebra de linha
-	std::cout << "Frase Preferida: Seja você quem for, seja qual for a posição social que você tenha na vida, a mais alta ou a mais baixa, tenha sempre como meta muita força, muita determinação e sempre faça tudo com muito amor e com muita fé em Deus, que um dia você chega lá. De alguma maneira você chega lá." << std::endl;
+	std::cout
+		<< "Frase Preferida: Seja você quem for, seja qual for a posição social que você tenha na vida, a mais alta ou a mais baixa, tenha sempre como meta muita força, muita determinação e sempre faça tudo com muito amor e com muita fé em Deus, que um dia você chega lá. De alguma maneira você chega lá." << "\n"
+		<< "----------------------------------------------------------------------------" << "\n"
+		<< "" << "\n"
+		<< "Eu" << "\n"
+		<< "Vou" << "\n"
+		<< "Aprender" << "\n"
+		<< "C++" << "\n";
 	system("color 0B");
-	system("PAUSE");
+	// Opcional no VS 2022.
+	/*system("PAUSE");*/
 	// chama o shell do windows e coloca o comando pause, funciona maiúscula e minúscula nesse config.
 	return 0;
 	// return 0 é o comando de retorno da função principal para indicar que o programa foi executado com sucesso
