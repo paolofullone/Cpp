d869fcc33ebd1ec98dc3c0ccae61045a04d0af14
paolofullone <paolo.enrico@gmail.com>
2023-03-10 07:30:05 -0300

    ponteiros

---
+++ Marcos/Basica_Avancada/107-VariavelPonteiro/Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+
+	// um ponteiro também é uma variável, que armazenda dentro dele o endereço de memória.
+	// então para declarar uma variável que seja considerada um ponteiro
+	// basta colocar <tipo a ser apontado> *<nome da variável>
+	int* ptr;
+	//obs: a posição do asterisco neste caso não importa
+	//int *ptr; //dá no mesmo!
+
+
+	int Numero = 101;
+	//Aqui vamos colocar na tela o endereço de memória
+	//da variável Número. Ou seja onde ela está carregada na memória RAM. Lembre-se que o que vai para tela é o primeiro endereço da variável número
+	//pois ela ocupa 4 endereços pois tem 4 bytes de tamanho
+
+	std::cout << "\nEndereço de Número: " << &Numero;
+	//ALGO IMPORTANTE AQUI: &ptr vai colocar na tela o endereço de memória alocado para variável ponteiro
+	//ou seja qual lugar da RAM ele está alocado?
+	//veja nós ainda não estamos colocando na tela nada que esteja dentro das variáveis.
+	//pois para colocar o que está dentro das variáveis
+	//nós usamos apenas o nome da variável!
+	//exemplo std::cout << Numero
+	//std::cout << ptr
+
+	std::cout << "\nEndereço de ptr: " << &ptr << "\n";
+
+	// e como não definimos nenhum valor para ptr:
+	std::cout << "\nValor de ptr: " << ptr << "\n";
+
+	// printa um monte de CCCCCCCC, wtf?
+
+	system("PAUSE");
+	return 0;
+}
